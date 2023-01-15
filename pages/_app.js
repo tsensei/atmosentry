484985b84f253824393112d5907aa30ba001369f
tsensei <tjsiam019@gmail.com>
2023-01-15 09:42:43 +0600

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,17 @@
 import "../styles/globals.css";
 import { Montserrat } from "@next/font/google";
-// import { Raleway } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const montserrat = Montserrat({
   weight: "variable",
   subsets: ["cyrillic"],
 });
 
-// const raleway = Raleway({
-//   weight: "variable",
-//   subsets: ["latin"],
-// });
-
 export default function App({ Component, pageProps }) {
   return (
     <main className={montserrat.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
